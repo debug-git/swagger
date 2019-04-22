684bbd93a42446f60911d94177b1b7c184b1caba
aboutme <947068981@qq.com>
2019-04-23 00:04:46 +0800

    增加拦截器匹配规则描述

--- src/main/java/com/example/swagger2/configuration/MyWebConfig.java
+++ src/main/java/com/example/swagger2/configuration/MyWebConfig.java
@@ -28,6 +28,12 @@ public class MyWebConfig implements WebMvcConfigurer {
         registry.addViewController("/index").setViewName("/index.html");
     }
 
+
+    /**
+     - /**： 匹配所有路径
+     - /admin/**：匹配 /admin/ 下的所有路径
+     - /secure/*：只匹配 /secure/user，不匹配 /secure/user/info
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(interCeptor).addPathPatterns("/asd/**");
