1b64518423100c8a9647e26f5b4fd45ef315d15b
aboutme <947068981@qq.com>
2019-04-27 01:07:42 +0800

    提交一些无用的东西

--- src/main/java/com/example/swagger2/configuration/Env.java
+++ src/main/java/com/example/swagger2/configuration/Env.java
@@ -1,20 +1,30 @@
 package com.example.swagger2.configuration;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.jsp.PageContext;
 
 @Data
+@Configuration
 public class Env {
+    @Autowired
     private HttpServletRequest request;
+    @Autowired
     private HttpServletResponse response;
 //    private PageContext pageContext;
+    @Autowired
     private ServletContext servletContext;
+    @Autowired
+    private ApplicationContext context;
 
     /*
      * Bean factory part
@@ -26,4 +36,9 @@ public class Env {
             return null;
         }
     }
+
+//    public static getEnv(){
+//
+//    }
+
 }
--- src/main/java/com/example/swagger2/controller/IndexController.java
+++ src/main/java/com/example/swagger2/controller/IndexController.java
@@ -1,23 +1,38 @@
 package com.example.swagger2.controller;
 
+import com.example.swagger2.configuration.Env;
 import com.example.swagger2.configuration.MySessionListener;
-import com.example.swagger2.exception.ServiceEnum;
-import com.example.swagger2.exception.ServiceException;
 import com.example.swagger2.vo.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Api(tags = "这是一个启动action")
 @RestController
 public class IndexController {
     @Value("${value}")
     private String value;
+    @Autowired
+    private HttpServletRequest request;
+    @Autowired
+    private HttpServletResponse response;
+    @Autowired
+    private ServletContext servletContext;
+    @Autowired
+    private ApplicationContext context;
+    @Autowired
+    private Env Eenv;
 
     @ApiOperation(value = "用于进入首页", notes = "这里写一些备注说明,比如姓名不能为空")
     @ApiImplicitParam(name = "addr", value = "这是地址")
@@ -35,6 +50,15 @@ public class IndexController {
 
     @GetMapping("/online")
     public Integer getOnlineCount(){
+        System.out.println(request.getRequestURI());
+        System.out.println(request.getContextPath());
+        System.out.println(request.getAttribute("jjj"));
+        ServletContext servletContext = request.getServletContext();
+        ApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(servletContext);
+        IndexController f = context.getBean(IndexController.class);
+
+        HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
+
         return MySessionListener.count;
     }
 
